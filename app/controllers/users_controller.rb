258cfd4bd8ab0eba9fd78536b83db8285faca3fb
Michael Carroll <carrollm2@gmail.com>
2020-04-25 02:40:42 +0000

    add post sign up and login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,43 @@ class UsersController < ApplicationController
   end
   
   
+  post '/signup' do
+
+    if params[:username] == "" or params[:email] == "" or params[:password] == ""
+      redirect 'users/signup'
+    end
+
+    user = User.create(params)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/welcome"
+	  end
+  end
+
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect '/welcome'
+    else
+      erb :'/users/login'
+    end
+
+  end
+
+
+  post '/login' do
+
+    user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect '/welcome'
+	  else
+	    redirect '/users/login'
+	  end
+  end  
+  
+  
+  
+  
 end
