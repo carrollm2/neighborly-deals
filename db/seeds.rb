e7380ee9228de9562ea731b18546c50c4ed79635
Michael Carroll <carrollm2@gmail.com>
2020-04-26 02:58:37 +0000

    comment out user seed data for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-users_list = {
-    "Bart Simpspon" => {
-      :username => "bsimpson",
-      :email => "bsimpson@spfld.com",
-      :password => "donthaveacowman"
-    }
-}
+# users_list = {
+#     "Bart Simpspon" => {
+#       :username => "bsimpson",
+#       :email => "bsimpson@spfld.com",
+#       :password => "donthaveacowman"
+#     }
+# }
 
 
-users_list.each do |name, user_hash|
-  User.create(user_hash)
-end
+# users_list.each do |name, user_hash|
+#   User.create(user_hash)
+# end
 
 
 
