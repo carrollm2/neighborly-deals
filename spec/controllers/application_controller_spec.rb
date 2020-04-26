884a864954af47e522ec76bce55f93c48e86d47e
Michael Carroll <carrollm2@gmail.com>
2020-04-26 03:01:09 +0000

    add tests for user sign up, login and logout

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -17,7 +17,127 @@ describe ApplicationController do
       get '/signup'
       expect(last_response.status).to eq(200)
     end
+
+  
+    it 'does not let a user sign up without a username' do
+      params = {
+        :username => "",
+        :email => "skittles@aol.com",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end  
+  
+  
+    it 'does not let a user sign up without an email' do
+      params = {
+        :username => "skittles123",
+        :email => "",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end  
+    
+    
+    it 'does not let a user sign up without a password' do
+      params = {
+        :username => "skittles123",
+        :email => "skittles@aol.com",
+        :password => ""
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end    
+    
+    
+    it 'does not let a logged in user view the signup page' do
+      #user = User.create(:username => "skittles123", :email => "skittles@aol.com", :password => "rainbows")
+      params = {
+        :username => "skittles123",
+        :email => "skittles@aol.com",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      get '/signup'
+      expect(last_response.location).to include('/requests')
+    end
+  
   end
   
   
+  describe "login" do
+    it 'loads the login page' do
+      get '/login'
+      expect(last_response.status).to eq(200)
+    end  
+  
+ 
+    it 'loads the requests index after login' do
+      user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+      params = {
+        :username => "becky567",
+        :password => "kittens"
+      }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome,")
+    end 
+    
+    
+    it 'does not let user view login page if already logged in' do
+      user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+
+      params = {
+        :username => "becky567",
+        :password => "kittens"
+      }
+      post '/login', params
+      get '/login'
+      expect(last_response.location).to include("/requests")
+    end    
+  end
+  
+  describe "logout" do
+    it "lets a user logout if they are already logged in and redirects to the login page" do
+      user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+
+      params = {
+        :username => "becky567",
+        :password => "kittens"
+      }
+      post '/login', params
+      get '/logout'
+      expect(last_response.location).to include("/login")
+    end
+    
+    
+    it 'redirects a user to the index page if the user tries to access /logout while not logged in' do
+      get '/logout'
+      expect(last_response.location).to include("/")
+    end    
+    
+    
+    it 'redirects a user to the login route if a user tries to access /requests route if user not logged in' do
+      get '/requests'
+      expect(last_response.location).to include("/login")
+      expect(last_response.status).to eq(302)
+    end    
+    
+    
+    it 'loads /requests if user is logged in' do
+      user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+
+      visit '/login'
+
+      fill_in(:username, :with => "becky567")
+      fill_in(:password, :with => "kittens")
+      click_button 'submit'
+      expect(page.current_path).to eq('/requests')
+      expect(page.body).to include("Welcome")
+    end    
+  end
 end
