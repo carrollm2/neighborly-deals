c3025a643c0ffacb4e55245d168383a9470a9659
Michael Carroll <carrollm2@gmail.com>
2020-04-30 03:36:35 +0000

    added edit route and patch and delete requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -11,6 +11,7 @@ class RequestsController < ApplicationController
   
   get '/requests/new' do
     if Helpers.is_logged_in?(session)
+      @categories = ['grocery', 'automotive', 'fitness', 'other']
       erb :'requests/new'
     else
       redirect '/login'
@@ -26,6 +27,57 @@ class RequestsController < ApplicationController
         @user_request = Request.create(category: params["category"], description: params["description"], rtype: params["type"], user_id: session[:user_id])
         erb :'requests/index'
       end
+    end
+  end  
+  
+  
+  get '/requests/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_request = Request.find_by(id: params[:id])
+      erb :'requests/show'
+    else
+      redirect '/login'
+    end
+  end  
+  
+  
+  get '/requests/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @user_request = Request.find_by(id: params[:id])
+      @user = User.find_by(id: @user_request.user_id)
+      if @user.id == session[:user_id] && @user_request.category != "" && @user_request.description != "" && @user_request.rtype != ""
+        erb :"requests/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end  
+  
+  
+  patch '/requests/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_request = Request.find_by(id: params[:id])
+      @user = User.find_by(id: @user_request.user_id)
+      if @user.id == session[:user_id] && @user_request.category != "" && @user_request.description != "" && @user_request.rtype != ""
+        @user_request.update(category: params["request"]["category"])
+        @user_request.update(description: params["request"]["description"])
+        @user_request.update(rtype: params["request"]["type"])
+        redirect "requests/#{@user_request.id}/edit"
+      end
+    end
+  end
+  
+  
+  delete '/requests/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_request = Request.find_by(id: params[:id])
+      @user = User.find_by(id: @user_request.user_id)
+      if @user.id == session[:user_id]
+        @user_request.delete
+        redirect "/requests"
+      end
+    else
+      redirect '/login'
     end
   end  
   
