03b3487c9bd73d46c2fcc9ee0dc83588a064ea53
Michael Carroll <carrollm2@gmail.com>
2020-05-05 01:45:44 +0000

    removed commented out code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
-# users_list = {
-#     "Bart Simpspon" => {
-#       :username => "bsimpson",
-#       :email => "bsimpson@spfld.com",
-#       :password => "donthaveacowman"
-#     }
-# }
-
-
-# users_list.each do |name, user_hash|
-#   User.create(user_hash)
-# end
-
 category_list = {
     "grocery" => {
     },
