a23a94e9038c68f54be834c73d516cc0031015ba
Michael Carroll <carrollm2@gmail.com>
2020-05-08 01:30:55 +0000

    replace Request model with Post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,89 @@
+class PostsController < ApplicationController
+
+  get '/posts' do
+    if Helpers.is_logged_in?(session)
+      @user_name = User.find_by(id: session[:user_id]).username      
+      erb :'posts/index'
+    else
+      redirect '/login'
+    end
+  end
+  
+  
+  get '/posts/new' do
+    if Helpers.is_logged_in?(session)
+      erb :'posts/new'
+    else
+      redirect '/login'
+    end
+  end  
+  
+  
+  post '/posts' do
+    if Helpers.is_logged_in?(session)
+      if params["category"]["name"] == "" || params["description"] == "" || params["post"]["type"] == ""
+        redirect 'posts/new'
+      else
+        @user_name = User.find_by(id: session[:user_id]).username
+        @user_post = Post.create(category: params["category"]["name"], description: params["description"], rtype: params["post"]["type"], user_id: session[:user_id])
+        
+        flash[:message] = "Successfully created post."
+        redirect to "/posts/#{@user_post.id}"
+      end
+    end
+  end  
+  
+  
+  get '/posts/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_post = Post.find_by(id: params[:id])
+      erb :'posts/show'
+    else
+      redirect '/login'
+    end
+  end  
+  
+  
+  get '/posts/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @user_post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @user_post.user_id)
+      if @user.id == session[:user_id] && @user_post.category != "" && @user_post.description != "" && @user_post.rtype != ""
+        erb :"posts/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end  
+  
+  
+  patch '/posts/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @user_post.user_id)
+      if @user.id == session[:user_id] && @user_post.category != "" && @user_post.description != "" && @user_post.rtype != ""
+        @user_post.update(category: params["post"]["category"])
+        @user_post.update(description: params["post"]["description"])
+        @user_post.update(rtype: params["post"]["type"])
+        
+        flash[:message] = "Successfully updated post."
+        
+        redirect "posts/#{@user_post.id}"
+      end
+    end
+  end
+  
+  
+  delete '/posts/:id' do
+    if Helpers.is_logged_in?(session)
+      @user_post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @user_post.user_id)
+      if @user.id == session[:user_id]
+        @user_post.delete
+        redirect "/posts"
+      end
+    else
+      redirect '/login'
+    end
+  end  
+end
