5010469ed7c34e2727f32fd9b4648fd399531c03
Michael Carroll <carrollm2@gmail.com>
2020-06-06 03:25:19 +0000

    renamed instance variable names to @username and @post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   get '/posts' do
     if Helpers.is_logged_in?(session)
-      @user_name = User.find_by(id: session[:user_id]).username      
+      @username = User.find_by(id: session[:user_id]).username      
       erb :'posts/index'
     else
       redirect '/login'
@@ -24,11 +24,11 @@ class PostsController < ApplicationController
       if params["category"]["id"] == "" || params["description"] == "" || params["post_type"]["id"] == ""
         redirect 'posts/new'
       else
-        @user_name = User.find_by(id: session[:user_id]).username
-        @user_post = Post.create(category_id: params["category"]["id"], description: params["description"], post_type_id: params["post_type"]["id"], user_id: session[:user_id])
+        @username = User.find_by(id: session[:user_id]).username
+        @post = Post.create(category_id: params["category"]["id"], description: params["description"], post_type_id: params["post_type"]["id"], user_id: session[:user_id])
         
         flash[:message] = "Successfully created post."
-        redirect to "/posts/#{@user_post.id}"
+        redirect to "/posts/#{@post.id}"
       end
     end
   end  
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   
   get '/posts/:id' do
     if Helpers.is_logged_in?(session)
-      @user_post = Post.find_by(id: params[:id])
+      @post = Post.find_by(id: params[:id])
       erb :'posts/show'
     else
       redirect '/login'
@@ -46,9 +46,9 @@ class PostsController < ApplicationController
   
   get '/posts/:id/edit' do
     if Helpers.is_logged_in?(session)
-      @user_post = Post.find_by(id: params[:id])
-      @user = User.find_by(id: @user_post.user_id)
-      if @user.id == session[:user_id] && @user_post.category_id != "" && @user_post.description != "" && @user_post.post_type_id != ""
+      @post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @post.user_id)
+      if @user.id == session[:user_id] && @post.category_id != "" && @post.description != "" && @post.post_type_id != ""
         erb :"posts/edit"
       else
         flash[:message] = "Not authorized to edit post of other users."
@@ -62,16 +62,16 @@ class PostsController < ApplicationController
   
   patch '/posts/:id' do
     if Helpers.is_logged_in?(session)
-      @user_post = Post.find_by(id: params[:id])
-      @user = User.find_by(id: @user_post.user_id)
-      if @user.id == session[:user_id] && @user_post.category_id != "" && @user_post.description != "" && @user_post.post_type_id != ""
-        @user_post.update(category_id: params["post"]["category_id"])
-        @user_post.update(description: params["post"]["description"])
-        @user_post.update(post_type_id: params["post_type"]["id"])
+      @post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @post.user_id)
+      if @user.id == session[:user_id] && @post.category_id != "" && @post.description != "" && @post.post_type_id != ""
+        @post.update(category_id: params["post"]["category_id"])
+        @post.update(description: params["post"]["description"])
+        @post.update(post_type_id: params["post_type"]["id"])
         
         flash[:message] = "Successfully updated post."
         
-        redirect "posts/#{@user_post.id}"
+        redirect "posts/#{@post.id}"
       end
     end
   end
@@ -79,10 +79,10 @@ class PostsController < ApplicationController
   
   delete '/posts/:id' do
     if Helpers.is_logged_in?(session)
-      @user_post = Post.find_by(id: params[:id])
-      @user = User.find_by(id: @user_post.user_id)
+      @post = Post.find_by(id: params[:id])
+      @user = User.find_by(id: @post.user_id)
       if @user.id == session[:user_id]
-        @user_post.delete
+        @post.delete
         redirect "/posts"
       end
     else
