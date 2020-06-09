c1cbc9b9f0225686884cd156032180749de1806c
Michael Carroll <carrollm2@gmail.com>
2020-06-09 03:00:39 +0000

    user is_valid_edit form helper in patch request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
     if Helpers.is_logged_in?(session)
       @post = Post.find_by(id: params[:id])
       @user = User.find_by(id: @post.user_id)
-      if @user.id == session[:user_id] && @post.category_id != "" && @post.description != "" && @post.post_type_id != ""
+      if @user.id == session[:user_id] && Helpers.is_valid_edit_form?(@post)
         @post.update(category_id: params["post"]["category_id"])
         @post.update(description: params["post"]["description"])
         @post.update(post_type_id: params["post_type"]["id"])
