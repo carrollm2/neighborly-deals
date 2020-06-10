d0e891a6cb387d08acc12dd2460fc87fa6a11d41
Michael Carroll <carrollm2@gmail.com>
2020-06-10 02:46:16 +0000

    added authorization to create new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,40 @@
 class CategoriesController < ApplicationController
 
   get '/categories' do
-    @categories = Category.all
-    erb :'/categories/index'
+    
+    if Helpers.is_admin_logged_in?(session)
+      erb :'/categories/index'
+    else
+      redirect '/posts'
+    end 
+    
   end
 
+
   get '/categories/new' do
-    erb :'/categories/new'
+    
+    if Helpers.is_admin_logged_in?(session)
+      erb :'categories/new'
+    else
+      redirect '/categories'
+    end    
+    
   end
+
 
   post '/categories' do
 
-    if !params["category"]["name"].empty?
-      Category.find_or_create_by(params["category"]["name"])
+    if Helpers.is_logged_in?(session)
+      if !params["category"]["name"].empty?
+        Category.find_or_create_by(name: params["category"]["name"])
+        erb :'categories/index'
+      else
+        erb :'categories/new'
+      end
+    else
+      redirect '/posts'
     end
-
-    redirect to "/categories"
-
+    
   end
+  
 end
