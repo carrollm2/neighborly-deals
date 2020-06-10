0bcb80a266204080bb8d62099315d760e5afbc8a
Michael Carroll <carrollm2@gmail.com>
2020-06-10 02:29:06 +0000

    added authorization check before new post_types can be created and saved

--- app/controllers/post_types_controller.rb
+++ app/controllers/post_types_controller.rb
@@ -1,20 +1,32 @@
 class PostTypesController < ApplicationController
 
   get '/post_types' do
-    erb :'/post_types/index'
+    if Helpers.is_admin_logged_in?(session)
+      erb :'post_types/index'
+    else
+      redirect '/posts'
+    end
   end
 
   get '/post_types/new' do
-    erb :'/post_types/new'
+    if Helpers.is_admin_logged_in?(session)
+      erb :'post_types/new'
+    else
+      redirect '/post_types'
+    end
   end
 
   post '/post_types' do
-
-    if !params["post"]["description"].empty?
-      PostType.find_or_create_by(params["post"]["description"])
+  
+    if Helpers.is_logged_in?(session)
+      if !params["post_type"]["description"].empty?
+        PostType.find_or_create_by(description: params["post_type"]["description"])
+        erb :'post_types/index'
+      else
+        erb :'post_types/new'
+      end
+    else
+      redirect '/posts'
     end
-
-    redirect to "/post_types"
-
   end
 end
