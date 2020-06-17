1db614d72721e8535edf103dd6a5f91a1dfd300e
Michael Carroll <carrollm2@gmail.com>
2020-06-17 03:07:49 +0000

    have logged_in reference current_user method

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,14 +1,14 @@
 class Helpers
   
   def self.current_user(session)
-    user = User.find(session[:user_id])
+    user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 
   def self.is_logged_in?(session)
-    !!session[:user_id]
+    !!current_user(session)
   end
-  
+
   
   def self.is_admin_logged_in?(session)
     session[:user_id] == 1
